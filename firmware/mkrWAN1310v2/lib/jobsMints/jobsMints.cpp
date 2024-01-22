6bdca0a3af0a41bd2395b0e0c2b5a0ec17f848f4
Lakitha Omal Harindha Wijeratne <lhw150030@utdallas.edu>
2024-01-21 20:22:14 -0600

    Update jobsMints.cpp
    Adding the delay into unavailable sensors

--- firmware/mkrWAN1310v2/lib/jobsMints/jobsMints.cpp
+++ firmware/mkrWAN1310v2/lib/jobsMints/jobsMints.cpp
@@ -11,7 +11,7 @@ void readRunner(int (*readFunction)(), String sensorID, bool sensorOnline, uint3
   }else{
     Serial.print(sensorID);
     Serial.print(" Offline");
-    
+    delay(sensingPeriod);
   }
 }
 
@@ -27,6 +27,7 @@ void readRunnerBool(int (*readFunction)(bool), String sensorID, bool sensorOnlin
   }else{
     Serial.print(sensorID);
     Serial.print(" Offline");
+    delay(sensingPeriod);
   }
 }
 
